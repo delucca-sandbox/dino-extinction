d72eb411611045a8d01f93240a1be9ed497f8a31
odelucca <delucca@pm.me>
2019-05-21 11:03:26 -0300

    wip

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,12 @@ from gevent.pywsgi import WSGIServer
 from dino_extinction import create_app
 
 if __name__ == '__main__':
+    default_port = 8080
     app = create_app()
-    WSGIServer(('0.0.0.0', 8080), app).serve_forever()
+    server = WSGIServer(('0.0.0.0', default_port), app)
+
+    art_file = open('ascii.txt')
+    art = art_file.read()
+    print(f"\033[92m{art}")
+    print(f"\033[94m\033[1m===> Server up and running on port {default_port}\033[0m")
+    server.serve_forever()
